8fdca419974dd2a089e3d8d2fccb8864cd013510
t.schroeer@double-d-it.de <t.schroeer@double-d-it.de>
2024-07-29 13:27:34 +0200

    Erster Versuch TreeView in Mitgliederverwaltung

--- src/app/module/mitgliederverwaltung/mitgliederverwaltung.module.ts
+++ src/app/module/mitgliederverwaltung/mitgliederverwaltung.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+// import {CollectionViewer, SelectionChange, DataSource} from '@angular/cdk/collections';
+// import {FlatTreeControl} from '@angular/cdk/tree';
 import { MitgliederverwaltungRoutingModule } from './mitgliederverwaltung-routing.module';
 import { MitgliederverwaltungComponent } from './mitgliederverwaltung.component';
+// import {BehaviorSubject, merge, Observable} from 'rxjs';
+// import {map} from 'rxjs/operators';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTreeModule} from '@angular/material/tree';
 
 
 @NgModule({
@@ -11,7 +18,8 @@ import { MitgliederverwaltungComponent } from './mitgliederverwaltung.component'
   ],
   imports: [
     CommonModule,
-    MitgliederverwaltungRoutingModule
+    MitgliederverwaltungRoutingModule,
+    MatTreeModule, MatButtonModule, MatIconModule, MatProgressBarModule
   ]
 })
 export class MitgliederverwaltungModule { }
