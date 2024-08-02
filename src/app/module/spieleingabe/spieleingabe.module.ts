379101e53dce5fa68ec45ec5ef586d48e30a2f52
t.schroeer@double-d-it.de <t.schroeer@double-d-it.de>
2024-08-02 12:05:19 +0200

    Anfang Spieleingabe

--- src/app/module/spieleingabe/spieleingabe.module.ts
+++ src/app/module/spieleingabe/spieleingabe.module.ts
@@ -4,6 +4,10 @@ import { CommonModule } from '@angular/common';
 import { SpieleingabeRoutingModule } from './spieleingabe-routing.module';
 import { SpieleingabeComponent } from './spieleingabe.component';
 
+import {MatCardModule} from '@angular/material/card';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -11,7 +15,9 @@ import { SpieleingabeComponent } from './spieleingabe.component';
   ],
   imports: [
     CommonModule,
-    SpieleingabeRoutingModule
+    SpieleingabeRoutingModule,
+
+    MatCardModule,MatTabsModule,MatStepperModule,MatButtonModule
   ]
 })
 export class SpieleingabeModule { }
